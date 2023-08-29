c8a22694ffb0afea00e135ab3150a69b132ab10b
Jeonghyun <chlwjdgus822@gmail.com>
2023-08-29 19:07:17 +0900

    2023.08.29 연습한거 추가

--- Java_Basic/src/oop/string/StringQuiz02.java
+++ Java_Basic/src/oop/string/StringQuiz02.java
@@ -35,18 +35,33 @@ public class StringQuiz02 {
 		}
 		*/
 		
-		while(true) {
-			System.out.println("주민등록번호 13자리를 입력해주세요(- 포함)\n> ");
-			
+		outer: while(true) {
+			System.out.println("주민등록번호 13자리를 입력하세요.");
+			System.out.println("-을 포함해서 입력하세요.");
+			System.out.print("> ");
 			String ssn = sc.next();
 			
 			ssn = ssn.replace("-", "");
 			
 			if(ssn.length() != 13) {
 				System.out.println("주민등록번호는 13자리 입니다.");
-			}else {
-//				switch
+			} else {
+				switch(ssn.charAt(6)) {
+				case '1': case '3':
+					System.out.println("남자입니다.");
+					break outer;
+				
+				case '2': case '4':
+					System.out.println("여자입니다.");
+					break outer;
+					
+				default:
+					System.out.println("잘못 입력했습니다.");
+				}
 			}
+			
 		}
+		
+		sc.close();
 	}
 }
